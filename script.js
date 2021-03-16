8de98a91491eff60527fa5aa961d89625ba6d30a
enzo-rs <enzorojoall@gmail.com>
2021-03-16 17:21:21 +0100

    Finish footer

--- script.js
+++ script.js
@@ -9,9 +9,11 @@ icon_burger.addEventListener('click',() => {
     menu.classList.toggle('show');
     if (count%2 === 0) {
         icon_burger.setAttribute("src", "images/icon-hamburger.svg");
+        document.body.style.overflow = "visible";
     }
     else {
         icon_burger.setAttribute("src", "images/icon-close.svg");
+        document.body.style.overflow = "hidden";
     }
  
     
